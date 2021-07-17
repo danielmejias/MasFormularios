e09d68420a5dabe01a7816ed07f70b0db488b6ad
danielmejias <82547292+danielmejias@users.noreply.github.com>
2021-07-17 15:23:53 -0400

    Adding work PropItUp

--- src/App.js
+++ src/App.js
@@ -6,28 +6,16 @@ function App() {
   return (
     <div class="centered">
       <PersonCard
-        lastName={"Apellido1"}
-        firstName={"Nombre1"}
-        age={10}
-        hairColor={"Brown"}
-      />
-      <PersonCard
-        lastName={"Apellido2"}
-        firstName={"Nombre2"}
-        age={20}
+        lastName={"Doe"}
+        firstName={"Joe"}
+        age={45}
         hairColor={"Black"}
       />
       <PersonCard
-        lastName={"Apellido3"}
-        firstName={"Nombre3"}
-        age={30}
-        hairColor={"Red"}
-      />
-      <PersonCard
-        lastName={"Apellido4"}
-        firstName={"Nombre4"}
-        age={40}
-        hairColor={"Blonde"}
+        lastName={"Smith"}
+        firstName={"John"}
+        age={88}
+        hairColor={"Brown"}
       />
     </div>
   );
--- src/componentes/Example2.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import Example from "./Example";
-function Example2() {
-  const ChildComp = ({ props }) => (
-    <button onClick={() => props("test string")}>Click Here</button>
-  );
-}
-export default Example2;
